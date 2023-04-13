389a147309f9298ceb5c319d3f1ed79f5fdb91bf
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-04-13 23:20:01 +0200

    changed 'session_key' to 'token' in 'create_session_table.sql'

--- internal/database/migrations/create_session _table.sql
+++ internal/database/migrations/create_session _table.sql
@@ -2,10 +2,10 @@
 CREATE TABLE IF NOT EXISTS `sessions` (
   `id` CHAR(36) NOT NULL,
   `user_id` CHAR(36) NOT NULL,
-  `session_key` VARCHAR(255) NOT NULL,
+  `token` VARCHAR(255) NOT NULL,
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `session_key` (`session_key`),
+  UNIQUE KEY `token` (`token`),
   FOREIGN KEY (`user_id`) REFERENCES `users`(`uuid`) ON DELETE CASCADE
 );
